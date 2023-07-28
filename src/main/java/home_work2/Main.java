5f3efab03298ef0ad03deccb187cd93fe9e5c858
Vladislav Sizion <Vladislav.Sizion@south.rt.ru>
2023-07-29 01:25:54 +0300

    home work 2 fixed

--- src/main/java/home_work2/Main.java
+++ src/main/java/home_work2/Main.java
@@ -1,24 +1,29 @@
 package home_work2;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] a1 = new int[]{1, 2, 3};
+        int[] a1 = new int[]{6, 2, 3};
         int[] a2 = new int[]{2, 2};
-        int[] a3 = new int[]{1, 1, 1, 1, 1};
+        int[] a3 = new int[]{1, 1, 7, 1, 1};
 
         System.out.println("1 Task ");
         System.out.println(Arrays.toString(getSumArrays(a1, a2, a3)));
 
-        int[] s = new int[]{5, 3, 4, -2};
+        int[] s1 = new int[]{5, 3, 4, -2};
+        int[] s2 = new int[]{1, 1, 1, 1, 1, 5};
+        int[] s3 = new int[]{7, 2, 2, 2};
+        int[] s4 = new int[]{9, 4};
 
         System.out.println("2 Task");
-        System.out.println((getExistDot(s) || getExistDot(getRevert(s)) ? "Exists Dot" : "Not exists Dot"));
+        System.out.println((getExistDot(s1) ? "Exists Dot" : "Not exists Dot"));
+        System.out.println((getExistDot(s2) ? "Exists Dot" : "Not exists Dot"));
+        System.out.println((getExistDot(s3) ? "Exists Dot" : "Not exists Dot"));
+        System.out.println((getExistDot(s4) ? "Exists Dot" : "Not exists Dot"));
 
-        int[] a = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        int[] a = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] b = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         int[] c = new int[]{100, 9, 8, 7, 78, 5, 4, 3, 2, 1};
 
@@ -35,50 +40,44 @@ public class Main {
 
     /*Task 1 get sum arrays*/
     public static int[] getSumArrays(int[] a, int[] b, int[] c) {
-        int[] sumArray;
-        if (a.length > b.length && a.length > c.length) {
-            sumArray = sumArrays(a, b);
-            sumArray = sumArrays(a, c);
-        } else if (b.length > c.length) {
-            sumArray = sumArrays(b, a);
-            sumArray = sumArrays(b, c);
-        } else {
-            sumArray = sumArrays(c, a);
-            sumArray = sumArrays(c, b);
+
+        int[] sumAr = new int[Math.max(Math.max(a.length, b.length), c.length)];
+        sumArray(sumAr, a);
+        sumArray(sumAr, b);
+        sumArray(sumAr, c);
+
+        return sumAr;
+    }
+
+    public static int[] sumArray(int[] a, int[] b) {
+        for (int i = 0; i < b.length; i++) {
+            a[i] = a[i] + b[i];
         }
-
-        return sumArray;
+        return a;
     }
-
-    /* Sum 2 arrays*/
-    public static int[] sumArrays(int[] a, int[] b) {
-        if (a.length > b.length) {
-            for (int i = 0; i < b.length; i++) {
-                a[i] = a[i] + b[i];
-            }
-        } else
-            for (int i = 0; i < a.length; i++) {
-                b[i] = b[i] + a[i];
-            }
-        return (a.length > b.length ? a : b);
-    }
-
 
     /*Task 2 get exists dot*/
     public static boolean getExistDot(int[] a) {
-        boolean existsDot = false;
+
 
         for (int i = 0; i < a.length; i++) {
-            int sumRithSide = 0;
-            for (int j = i + 1; j < a.length; j++) {
-                sumRithSide = sumRithSide + a[j];
+            int rSum = 0;
+            int lSum = 0;
+            for (int r = i + 1; r < a.length; r++) {
+                rSum = rSum + a[r];
             }
-            if (a[i] == sumRithSide) {
-                existsDot = true;
+
+            for (int l = 0; l <= i; l++) {
+                lSum = lSum + a[l];
             }
+
+            if (rSum == lSum && rSum != 0) {
+                return true;
+            }
+
         }
 
-        return existsDot;
+        return false;
     }
 
     /* Task 3 get asc or desc array */
@@ -105,10 +104,13 @@ public class Main {
 
     /* Task 4 get revert array*/
     public static int[] getRevert(int[] a) {
-        int[] b = a.clone();
+        int b;
 
-        for (int i = 0; i < a.length; i++) {
-            a[i] = b[b.length - i - 1];
+        for (int i = 0; i < a.length / 2; i++) {
+            b = a[i];
+            a[i] = a[a.length - i - 1];
+            a[a.length - i - 1] = b;
+
         }
 
         return a;
