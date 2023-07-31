28a2d6b35b70722b48194a97ac5e794d9a0267fb
Vladislav Sizion <Vladislav.Sizion@south.rt.ru>
2023-07-31 17:27:29 +0300

    home work 3 add opposite diagonal

--- src/main/java/home_work3/Main.java
+++ src/main/java/home_work3/Main.java
@@ -1,4 +1,4 @@
-package home_work_3;
+package home_work3;
 
 import static java.lang.Math.random;
 
@@ -84,7 +84,7 @@ public class Main {
     public static void setZeroDiagonal(int[][] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
-                if (i == j ) {
+                if (i == j || j == a.length-1 -i ) {
                     a[i][j] = 0;
 
                 }
