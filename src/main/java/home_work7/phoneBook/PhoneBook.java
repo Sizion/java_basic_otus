e1acd64255f39ef545cbe4296ecdfd9ce6a91cae
Vlad Sizion <vladislav.sizion@mail.ru>
2023-09-11 00:16:39 +0300

    home work 10 Collections 2

---
+++ src/main/java/home_work7/phoneBook/PhoneBook.java
@@ -0,0 +1,39 @@
+package home_work7.phoneBook;
+
+import java.util.*;
+
+public class PhoneBook {
+    HashMap<String, List<String>> phoneBook = new HashMap<>();
+
+    Set<String> setPhoneNumbers = new HashSet<>();
+
+    public PhoneBook() {
+    }
+
+    public void addUserPhone(String fio, String phoneNumber) {
+        if (!phoneBook.containsKey(fio)) {
+            List<String> listNumbers = new ArrayList<>();
+            listNumbers.add(phoneNumber);
+            phoneBook.put(fio, listNumbers);
+            setPhoneNumbers.add(phoneNumber);
+        } else {
+            phoneBook.get(fio).add(phoneNumber);
+            setPhoneNumbers.add(phoneNumber);
+        }
+    }
+
+    public List findUserNumbers(String fio) {
+        return phoneBook.get(fio);
+    }
+
+    public void containsPhoneNumber(String phoneNumber) {
+        System.out.println(setPhoneNumbers.contains(phoneNumber) ? "Exists - " +  phoneNumber : "Not exits - " + phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneBook{" +
+                "phoneBook=" + phoneBook +
+                '}';
+    }
+}
