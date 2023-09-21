db071776ce8f96633ef10417c033cbec031e2376
Vladislav Sizion <Vladislav.Sizion@south.rt.ru>
2023-09-21 22:22:41 +0300

    home work 14 create thread in for

--- src/main/java/home_work14/Main.java
+++ src/main/java/home_work14/Main.java
@@ -3,45 +3,59 @@ package home_work14;
 public class Main {
     public static void doubleArray() {
         long t = System.currentTimeMillis();
-        double[] arr = new double[500_000_000];
+        double[] arr = new double[40_000_000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
         }
         System.out.println("Time doubleArray = " + (System.currentTimeMillis() - t));
     }
 
-    public static void doubleArrayThread() throws InterruptedException {
+    public static void doubleArrayThread1() throws InterruptedException {
+        //long t = System.currentTimeMillis();
+        double[] arr = new double[40_000_000];
+
+        int countThread = 4;
+        int range = arr.length / countThread;
+
+        for (int i = 0; i < countThread; i++) {
+            int inc = i;
+            Thread thread = new Thread(() -> {
+                for (int j = inc * (range); j < (range) * (inc + 1); j++) {
+                    arr[j] = 1.14 * Math.cos(j) * Math.sin(j * 0.2) * Math.cos(j / 1.2);
+                }
+            });
+            thread.start();
+           // thread.join();
+        }
+
+       // System.out.println("Time doubleArrayThread1 = " + (System.currentTimeMillis() - t));
+
+    }
+
+    public static void doubleArrayThread2() throws InterruptedException {
         long t = System.currentTimeMillis();
-        double[] arr = new double[500_000_000];
+        double[] arr = new double[40_000_000];
 
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < arr.length/4; i++) {
+            for (int i = 0; i < arr.length / 4; i++) {
                 arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                //System.out.println("Thread 1 " + i);
             }
-            System.out.println("Thread 1 - " + arr.length/4);
         });
 
         Thread thread2 = new Thread(() -> {
-            for (int i = arr.length/4; i < arr.length/2; i++) {
+            for (int i = arr.length / 4; i < arr.length / 2; i++) {
                 arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                //System.out.println("Thread 2 " + i);
             }
-            System.out.println("Thread 2 -" + arr.length/2);
         });
         Thread thread3 = new Thread(() -> {
-            for (int i = arr.length/2; i < arr.length-arr.length/4; i++) {
+            for (int i = arr.length / 2; i < arr.length - arr.length / 4; i++) {
                 arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                //System.out.println("Thread 3 " + i);
             }
-            System.out.println("Thread 3 -" + (arr.length-arr.length/4));
         });
         Thread thread4 = new Thread(() -> {
-            for (int i = arr.length-arr.length/4; i < arr.length; i++) {
+            for (int i = arr.length - arr.length / 4; i < arr.length; i++) {
                 arr[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
-                //System.out.println("Thread 4 " + i);
             }
-            System.out.println("Thread 4 -" + arr.length);
         });
 
         thread1.start();
@@ -54,7 +68,7 @@ public class Main {
         thread3.join();
         thread4.join();
 
-        System.out.println("Time doubleArrayThread = " + (System.currentTimeMillis() - t));
+        System.out.println("Time doubleArrayThread2 = " + (System.currentTimeMillis() - t));
 
     }
 
@@ -62,6 +76,10 @@ public class Main {
 
         doubleArray();
 
-        doubleArrayThread();
+        long t = System.currentTimeMillis();
+        doubleArrayThread1();
+        System.out.println("Time doubleArrayThread1 = " + (System.currentTimeMillis() - t));
+
+        doubleArrayThread2();
     }
 }
